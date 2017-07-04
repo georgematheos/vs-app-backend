6e3f20b450edbc0ef4e045fc061884fa7ecaad53
George Matheos <gmatheos@exeter.edu>
2017-07-03 20:34:12 -0400

    got basic authentication working

---
+++ public/index.js
@@ -0,0 +1,28 @@
+const feathersRestClient = feathers()
+.configure(feathers.rest(location.origin).fetch(fetch));
+
+// Configure hooks
+feathersRestClient.configure(feathers.hooks());
+
+// Set up authentication abilities
+feathersRestClient.configure(feathers.authentication({
+  header: 'x-auth-token',
+  storage: window.localStorage
+}));
+
+console.log(feathersRestClient);
+
+// Authenticate the user
+feathersRestClient.authenticate({
+  strategy: 'local',
+  username: 'user',
+  password: 'pass'
+})
+.then(response => {
+  console.log('SUCCESSFUL AUTHENTICATION.  RESPONSE:');
+  console.log(response);
+})
+.catch(err => {
+  console.log('ERROR DURING AUTHENTICATION:');
+  console.log(err);
+});
