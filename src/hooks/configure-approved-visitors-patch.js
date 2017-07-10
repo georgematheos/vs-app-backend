653dab4a781f9b89bd7e6c0a961de693a1f2ecce
George Matheos <gmatheos@exeter.edu>
2017-07-10 19:36:00 -0400

    reformatted & improved add approved visitor

--- src/hooks/configure-approved-visitors-patch.js
+++ src/hooks/configure-approved-visitors-patch.js
@@ -12,14 +12,16 @@ const errors = require('feathers-errors');
 
 module.exports = function (options = {}) { // eslint-disable-line no-unused-vars
   return function (hook) {
+    const listOwnerUsername = hook.id;
+    const approvedVisitorUsername = hook.data.approvedVisitorUsername;
 
     // depending on the operation to be performed (hook.data.op), we do different things
     switch (hook.data.op) {
       case "addApprovedVisitor":
         // the only person allowed to add an approved visitor is the owner of the approved visitor list
         // check that this is actually that person and deny access if it isn't
-        if (hook.params.user.username !== hook.id) {
-          return Promise.reject(new errors.Forbidden('only a user with the username ' + hook.id + ' may perform this action'));
+        if (hook.params.user.username !== listOwnerUsername) {
+          throw new errors.Forbidden('only a user with the username `' + hook.id + '` may perform this action');
         }
 
         // if we get here, it is valid for this user to make this request
@@ -32,7 +34,7 @@ module.exports = function (options = {}) { // eslint-disable-line no-unused-vars
         const approvedVisitors = hook.app.service('approved-visitors');
 
         // find the info on the list owner specified in the URL
-        return approvedVisitors.find({ listOwnerUsername: hook.id })
+        return approvedVisitors.find({ listOwnerUsername: listOwnerUsername })
         .then(results => {
           // if no entry exists in the database for this user, note that we will have to create one
           if (results.total === 0) {
@@ -41,15 +43,19 @@ module.exports = function (options = {}) { // eslint-disable-line no-unused-vars
           // if an entry was returned, this is the approved visitor list data, so make note of the ID and data
           else {
             approvedVisitorsListId = results.data[0]._id;
-            console.log('yup');
-            console.log(results);
             approvedVisitorsList = results.data[0].approvedVisitors;
-            console.log(approvedVisitorsList)
+
+            // check that the person to add isn't already an approved visitor for the list owner
+            // if they are, throw an error
+            if (approvedVisitorsList.includes(approvedVisitorUsername)) {
+              throw new errors.Forbidden('the user with username `' + approvedVisitorUsername + '` is already an approved visitor for the user with username `' + listOwnerUsername + '`');
+            }
           }
 
           return hook;
         })
-        .then(hook => { // CREATE NEW LIST IF NECESSARY
+        /* create a new list if necessary */
+        .then(hook => {
           // only run this hook if we need to create a new list, so exit it if we don't
           if (!createNewList) {
             return hook;
@@ -66,28 +72,29 @@ module.exports = function (options = {}) { // eslint-disable-line no-unused-vars
             return hook;
           });
         })
+        /* configure hook object with properly formatted patch request */
         .then(hook => {
           // add the new approved visitor to the list of approved visitors
-          approvedVisitorsList.push(hook.data.approvedVisitorUsername);
+          approvedVisitorsList.push(approvedVisitorUsername);
 
           // set the ID of the entity to be modified, and the data to modify it with, to the correct values
           hook.id = approvedVisitorsListId;
           hook.data = { approvedVisitors: approvedVisitorsList }
         });
 
-        // no need for a break; statement since we have already returned something
+        // no need for a break; since we have already returned something
 
       case "removeApprovedVisitor":
         // the only people allowed to remove an approved visitor is the owner of the approved visitor list or the approved visitor to be removed
         // check that this is actually one of the 2 acceptable people and deny access if it isn't
         if (hook.params.user.username !== hook.id && hook.params.user.username !== hook.data.approvedVisitorUsername) {
-          return Promise.reject(new errors.Forbidden('only the user with a username ' + hook.id + ' or ' + hook.data.approvedVisitorUsername + ' may perform this action'));
+          throw new errors.Forbidden('only the user with a username `' + hook.id + '` or `' + hook.data.approvedVisitorUsername + '` may perform this action');
         }
         break;
 
       default:
         // if the operation isn't one enumerated above, we don't know how to deal with it, so it is a malformed request
-        return Promise.reject(new errors.BadRequest('the provided operation (op) was unrecognized'));
+        throw new errors.BadRequest('the provided operation (op: `' + hook.data.op + '`) was unrecognized');
     }
 
     return Promise.resolve(hook);
