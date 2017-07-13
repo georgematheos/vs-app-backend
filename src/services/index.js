230e6934126cb946109abd2d8e4e4e7a9fbc1f4f
George Matheos <gmatheos@exeter.edu>
2017-07-13 15:37:31 -0400

    generated service for approved visitor addition blocks

--- src/services/index.js
+++ src/services/index.js
@@ -1,7 +1,9 @@
 const users = require('./users/users.service.js');
 const approvedVisitors = require('./approved-visitors/approved-visitors.service.js');
+const approvedVisitorAdditionBlocks = require('./approved-visitor-addition-blocks/approved-visitor-addition-blocks.service.js');
 module.exports = function () {
   const app = this; // eslint-disable-line no-unused-vars
   app.configure(users);
   app.configure(approvedVisitors);
+  app.configure(approvedVisitorAdditionBlocks);
 };
